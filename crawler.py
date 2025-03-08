31fa1b1dbeccbe907fd6a49b0db3b21ef39972c3
Markus Böbel <markus.boebel@tum.de>
2025-03-08 12:37:46 +0100

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from supabase import create_client, Client
 from crawler.models import Mountain
 
 url: str = 'https://ntcmlxdemillsrdjybpc.supabase.co'
-key: str = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50Y21seGRlbWlsbHNyZGp5YnBjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MjA0MDgzMCwiZXhwIjoyMDA3NjE2ODMwfQ.yTQhbzzibLoXORx8sbdb35nl_lWRp0wxHgA3o8IoIPQ'
+key: str = ''
 
 supabase: Client = create_client(url, key)
 
